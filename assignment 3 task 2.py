58e96517334dad5b82abdc6f1989022b5d9efb26
Rochit1553 <rochitshegokar1553@gmail.com>
2025-07-22 15:18:12 +0530

    Add files via upload

---
+++ assignment 3 task 2.py
@@ -0,0 +1,6 @@
+number = float(input("enter a number"))
+
+import math
+print("the square root of",number,"is",math.sqrt(number))
+print("natural logarithm of the number",number,"is",math.log(number))
+print(("sine of the number",number ,"in radians  is",math.sin(number)))
